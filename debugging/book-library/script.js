a0563ad6f531b8d8f7bac45f7dd46bf39a5df4cf
tasleemadedokun <adedokuntasleem@gmail.com>
2026-04-13 08:40:56 +0100

    Change the html onclick submit to addbook and implement the function function addbook too.

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -27,7 +27,7 @@ const check = document.getElementById("check");
 
 //check the right input from forms and if its ok -> add the new book (object in array)
 //via Book function and start render function
-function submit() {
+function addBook() {
   if (
     title.value == null ||
     title.value == "" ||
