b5023677dc0728859390a3ee15812910a81f2cf5
tasleemadedokun <adedokuntasleem@gmail.com>
2026-04-13 16:07:20 +0100

    Implement new change.

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -93,10 +93,10 @@ function render() {
     //add delete button to every row and render again
     let delButton = document.createElement("button");
     delButton.id = i + 5;
-    deleteCell.appendChild(delBut);
+    deleteCell.appendChild(delButton);
     delButton.className = "btn btn-warning";
     delButton.innerHTML = "Delete";
-    delButton.addEventListener("clicks", function () {
+    delButton.addEventListener("click", function () {
       alert(`You've deleted title: ${myLibrary[i].title}`);
       myLibrary.splice(i, 1);
       render();
