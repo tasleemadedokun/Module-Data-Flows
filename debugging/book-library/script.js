86339e52f1e91938021c0cb495818ed8312d4e2e
tasleemadedokun <adedokuntasleem@gmail.com>
2026-04-13 16:46:08 +0100

    Remove the strings in numbers 252 and 127.

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -5,11 +5,11 @@ window.addEventListener("load", function () {
 });
 function populateStorage() {
   if (myLibrary.length == 0) {
-    let book1 = new Book("Robison Crusoe", "Daniel Defoe", "252", true);
+    let book1 = new Book("Robison Crusoe", "Daniel Defoe", 252, true);
     let book2 = new Book(
       "The Old Man and the Sea",
       "Ernest Hemingway",
-      "127",
+      127,
       true
     );
     myLibrary.push(book1);
